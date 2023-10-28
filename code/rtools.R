e2995c1ab684020bbc03caa7f193fdca5cb839f8
Duc Nguyen <46308688+tanduc307@users.noreply.github.com>
2023-10-28 15:01:43 +0700

    .

--- code/rtools.R
+++ code/rtools.R
@@ -22,3 +22,13 @@ pkgbuild::check_rtools(debug = FALSE)
 
 # https://github.com/rstudio/rstudio/issues/3563
 # https://www.h3xed.com/windows/how-to-add-to-and-edit-windows-path-variable
+
+
+#### Khi cần update thủ công vị trí đường dẫn Rtools, ta thay đổi giá trị ở file này
+.Renviron (sử dụng lệnh Sys.getenv("HOME") )
+
+# chèn dòng lệnh này vào
+
+PATH="${RTOOLS43_HOME}\usr\bin;${PATH}"
+# sau đó khởi động lại RStudio
+
